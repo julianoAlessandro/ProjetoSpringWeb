79831de6f6ad9e97038c3b89d65d095ab5a80716
Juliano Alessandro dos Santos <111141842+julianoAlessandro@users.noreply.github.com>
2026-01-07 11:22:31 -0300

    Juliano servico JPA SPRING BOOT 07/01.

--- ProjetoSpringWeb/src/main/java/com/educaweb/AppAplication.java
+++ ProjetoSpringWeb/src/main/java/com/educaweb/AppAplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppAplication {
     public static void main(String[] args) {
         SpringApplication.run(AppAplication.class, args);
+        System.out.println("Olaaaaaaaa");
     }
 }
